9a6e886c16818783809ddf1202fbabfe0c9b9db9
Ryland Bell <ryland.bell@gmail.com>
2016-12-22 11:45:56 -0800

    re-design header

--- static/javascripts/ui.js
+++ static/javascripts/ui.js
@@ -1,15 +1,10 @@
 $(document).ready(function () {
 
-  //mark current page as active in navbar:
-  var urlArray = window.location.pathname.split('/');
-  var urlLastStep = urlArray[urlArray.length-1];
-  // url = url.substring(1, url.length-1);
-  $('#' + urlLastStep).addClass('active');
+  //animated down-arrow in header:
+  var animateArrow = function animateArrow(){
+    $('.down-arrow').toggleClass('animated rubberBand')
+  }
 
-  //initialize Bootstrap popovers:
-  $(function () {
-    $('[data-toggle="popover"]').popover();
-  });
-
+  setInterval(animateArrow,1500);
 });
 
