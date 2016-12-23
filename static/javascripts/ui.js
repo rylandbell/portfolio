da3611c8b06e766d254d11028ff040731e41b9b1
Ryland Bell <ryland.bell@gmail.com>
2016-12-23 12:16:20 -0800

    small tweaks

--- static/javascripts/ui.js
+++ static/javascripts/ui.js
@@ -1,9 +1,20 @@
 $(document).ready(function () {
 
   //animated down-arrow in header:
-  var animateArrow = function animateArrow(){
-    $('.down-arrow').toggleClass('animated rubberBand')
+  function animateArrow(){
+    $('.down-arrow')
+      .first()
+      .toggleClass('animated rubberBand');
   }
+
+  $(window).scroll(function() {
+      var height = $(window).scrollTop();
+      if(height  > 100) {
+        $('a.navbar-brand')
+          .removeClass('hidden')
+          .addClass('animated fadeIn');
+      }
+  });
 
   setInterval(animateArrow,1000);
 });
